49840af4aaa41e0d95adf77fb01d769d34764b37
gky360 <gky360@gmail.com>
2017-12-29 05:20:27 +0900

    4_3_iris_whitening.py

---
+++ 4_3_iris_whitening.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+# In[]:
+iris = datasets.load_iris()
+data = iris.data
+print(iris.feature_names)
+print(data)
+
+# In[]:
+mu = np.mean(data[:, 2:4], axis=0)
+cov = np.cov(data[:, 2:4], rowvar=0)
+v, S = np.linalg.eig(cov)
+L = np.linalg.inv(S).dot(cov).dot(S).diagonal() * np.eye(2)
+L
+
+# In[]:
+data_wht = np.linalg.inv(L ** 0.5).dot(S.T).dot((data[:, 2:4] - mu).T).T
+
+# In[]:
+plt.figure(figsize=(6, 6))
+plt.scatter(data[  0: 50, 2], data[  0: 50, 3], marker='$s$')
+plt.scatter(data[ 50:100, 2], data[ 50:100, 3], marker='$c$')
+plt.scatter(data[100:150, 2], data[100:150, 3], marker='$v$')
+plt.xlim(1, 7)
+plt.ylim(-2, 4)
+plt.xlabel('petal length (cm)')
+plt.ylabel('petal width (cm)')
+plt.title('Before whitening')
+plt.show()
+
+# In[]:
+plt.figure(figsize=(6, 6))
+plt.scatter(data_wht[  0: 50, 0], data_wht[  0: 50, 1], marker='$s$')
+plt.scatter(data_wht[ 50:100, 0], data_wht[ 50:100, 1], marker='$c$')
+plt.scatter(data_wht[100:150, 0], data_wht[100:150, 1], marker='$v$')
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+plt.xlabel('petal length (cm)')
+plt.ylabel('petal width (cm)')
+plt.title('After whitening')
+plt.show()
